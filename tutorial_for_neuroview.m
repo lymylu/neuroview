b15a0cc86d23526449bb27134f079116bf4706a2
Aptt <yuelp@psych.ac.cn>
2025-04-21 13:42:35 +0800

    完成初步的教程调试,上传示例LFP数据

--- tutorial_for_neuroview.m
+++ tutorial_for_neuroview.m
@@ -13,17 +13,23 @@ extractdata=objmatrix.ExtractData('LFPdata',1,'EVTdata',1); % using the first LF
 % not that no channel or event was defined, here extract the whole channel and time of the LFPdata.
 
 % you can define the channel and event information using
-extractdata2=NeuroMethod.getParams(extractdata);
-% before read the data. 
+extractdata2=NeuroMethod.getParams(extractdata); 
+% open the interface to select the event and channel of the LFPdata,
+% choose the timepoint and -2 to 2 because the event file contains several
+% laser stimulus event.
+% channel and eventtype could be changed or select all of them.
 
 % Read data from the neurodata extractdata to generate the NeuroResult object for further analysis.
 neuroresult=extractdata2.ReadData();  % or extractdata2.ReadData(neuroresult) to read the data from an exist NeuroResult object.
+% see neuroresult.LFPdata for the extracted LFP epoches.
 
 % to apply the analysis method, just using
 % params=[AnalysisMethod].getParams and Analysis.cal(params,neuroresult,resultname)
 
 params=Spectrogram.getParams;
 neuroresult=Spectrogram.cal(params,neuroresult,'Spectrogram1');
+% here, the field 'Spectrogram1' in neuroresult is the transformation of
+% time-frequency domain of the extract LFP.
 
 % you can change the params and get another calculation as Spectrogram 2
 params2=Spectrogram.getParams;
@@ -31,11 +37,15 @@ neuroresult=Spectrogram.cal(params2,neuroresult,'Spectrogram2');
 
 % to save the calculation using neuroresult.Savedata(savepath,savefilename,format,varname)
 neuroresult.SaveData('.','sample_data','hdf5','sample');
+% a hdf5 file was added in './sample_data/sample'; note that if dir
+% './sample_data/sample' is exist, it will be not work to save.
+
 
 % to plot the result, use NeuroPlot.NeuroPlot
 figure;
 fig=NeuroPlot.NeuroPlot();
 fig.Plot(gcf,{fullfile('.','sample_data','sample')});
+% you can select the different events, channels to plot in the gui.
 %%
 % % % % % % % % % % PerieventHistogram method % % % % % % % % % %
 extractdata=objmatrix.Extractdata('SPKdata',1,'EVTdata',1);
